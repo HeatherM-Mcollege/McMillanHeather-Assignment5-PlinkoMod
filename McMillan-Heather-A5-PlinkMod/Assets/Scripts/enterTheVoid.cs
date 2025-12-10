cd9545bca0e42b2f57fa488b000adcacfdde765f
Heather M <heather.mcmillan3@mohawkcollege.ca>
2025-12-09 19:41:49 -0500

    Add Void Add program crash recorvery file

---
+++ McMillan-Heather-A5-PlinkMod/Assets/Scripts/enterTheVoid.cs
@@ -0,0 +1,33 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.Rendering.DebugUI.Table;
+
+public class enterTheVoid : MonoBehaviour
+{
+
+    //make audio sound variable 
+    public AudioClip voidSound;
+    //make refereence for audio
+    private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        //call audio tied to game object
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        if (audioSource != null && voidSound != null)
+        {
+            audioSource.PlayOneShot(voidSound);
+        }
+       
+        //destroy disc on void entry
+        Destroy(collider.gameObject);
+    }
+}
