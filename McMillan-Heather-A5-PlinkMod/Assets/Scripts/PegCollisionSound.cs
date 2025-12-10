d4bccd208568dd345f3db5116a9fdf0a2a07423a
Heather M <heather.mcmillan3@mohawkcollege.ca>
2025-12-09 19:12:24 -0500

    Add Sound

---
+++ McMillan-Heather-A5-PlinkMod/Assets/Scripts/PegCollisionSound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PegCollisionSound : MonoBehaviour
+{
+    //make audio sound variable 
+    public AudioClip discCollisionSound;
+    //make refereence for audio
+    private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        //call audio tied to game object
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    //create collision check of gameobject
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check for source and play audio once
+        if (audioSource != null && discCollisionSound != null)
+        {
+            audioSource.PlayOneShot(discCollisionSound);
+        }
+    }
+}
---
+++ McMillan-Heather-A5-PlinkMod/Assets/Scripts/scoreSound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class scoreSound : MonoBehaviour
+{
+    //make audio sound variable 
+    public AudioClip scoreAudio;
+    //make refereence for audio
+    private AudioSource audioSource;
+
+
+    private void Start()
+    {
+        //call audio tied to game object
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    //create collision check of gameobject
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //check for source and play audio once
+        if (audioSource != null && scoreAudio != null)
+        {
+            audioSource.PlayOneShot(scoreAudio);
+        }
+    }
+ 
+}
